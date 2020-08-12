851ef266a2ccc38f5aa55aca4cd526286d36ea3a
Xbigo <wing123fire@gmail.com>
2020-08-12 10:35:18 +0800

    Add test for threadpool

--- test/threadpool.cpp
+++ test/threadpool.cpp
@@ -1,16 +1,6 @@
 #include <catch2/catch.hpp>
 #include <ape/thread/threadpool.hpp>
 
-int foo(int x) { return x + 42; }
-void test(){
-
-	using namespace ape::thread;
-	auto tpool = thread_pool::create();
-	auto f = tpool->async(&foo, 4);
-	CHECK(f.get() == 42);
-	tpool->stop();
-}
-
 TEST_CASE("ape::thread::thread_pool", "[ape.thread.pool]"){
 	using namespace ape::thread;
 	using namespace std::chrono_literals;
@@ -23,7 +13,14 @@ TEST_CASE("ape::thread::thread_pool", "[ape.thread.pool]"){
 		CHECK(!default_thread_pool().is_stopped());
 		CHECK(default_thread_pool().workers() == 2);
 
-		auto f = async([](int x){return 42;}, 4);
+		auto f = async([](int a, int b){return a + b;}, 40, 2);
 		CHECK(f.get() == 42);
+	}
+	SECTION("Check thread pool object"){
+		auto tpool = thread_pool::create();
+		auto f = tpool->async([]{return 42;});
+		CHECK(f.get() == 42);
+
+		tpool->stop();
 	}
 }
