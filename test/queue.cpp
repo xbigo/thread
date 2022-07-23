26e3ac27e8658ae79371833ab7230bca795bfe5b
Xbigo <wing123fire@gmail.com>
2022-07-23 12:40:42 +0800

    Upgrade catch2 to 3.1.0

--- test/queue.cpp
+++ test/queue.cpp
@@ -1,4 +1,4 @@
-#include <catch2/catch.hpp>
+#include <catch2/catch_all.hpp>
 #include <ape/thread/queue.hpp>
 #include <thread>
 #include <atomic>
--- test/threadpool.cpp
+++ test/threadpool.cpp
@@ -1,4 +1,4 @@
-#include <catch2/catch.hpp>
+#include <catch2/catch_all.hpp>
 #include <ape/thread/threadpool.hpp>
 
 TEST_CASE("ape::thread::thread_pool", "[ape.thread.pool]"){
